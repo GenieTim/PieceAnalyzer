6d62847f983766800d5ddf4c7d0a7cec6deccb98
Tim Bernhard <tim@bernhard-webstudio.ch>
2018-02-13 08:47:36 +0100

    setup needed files

---
+++ src/Controller/ListController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ListController extends Controller
+{
+
+    /**
+     * @Route("/all")
+     */
+    public function listAllAction()
+    {
+
+    }
+}
---
+++ src/Controller/LoadController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class LoadController extends Controller {
+
+    public function loadSelectionAction() {
+        
+    }
+}
