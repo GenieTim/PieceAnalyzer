8e013a9a4bece4385c2856ce2bdc5dc22517d818
Tim Bernhard <tim@bernhard-webstudio.ch>
2018-04-22 14:33:25 +0200

    updated data

--- bin/update.sh
+++ bin/update.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+# Copyright (c) 2018 Tim Bernhard
+# Pulls from remote, installs dependencies and executes the update command.
 
 cd $(dirname $(dirname "$0"))
 
+git pull
 composer install --no-dev --optimize-autoloader
 php bin/console cache:clear --env=prod --no-debug
 php bin/console assets:install --env=prod --no-debug
 yarn run encore production
+./bin/fix-permissions.sh
