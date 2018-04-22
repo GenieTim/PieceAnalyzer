204829b165ef5ff08689be3acdbe6670057ffc55
Tim Bernhard <tim@bernhard-webstudio.ch>
2018-04-22 21:49:04 +0200

    import from csv in stages

--- src/Command/AppImportCsvCommand.php
+++ src/Command/AppImportCsvCommand.php
@@ -10,36 +10,50 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use App\Service\CsvLegoLoaderService;
 
-class AppImportCsvCommand extends Command
-{
+class AppImportCsvCommand extends Command {
+
     protected static $defaultName = 'app:import-csv';
     protected $loader;
-    
+
     public function __construct(CsvLegoLoaderService $loader) {
         $this->loader = $loader;
         parent::__construct();
     }
 
-    protected function configure()
-    {
+    protected function configure() {
         $this
-            ->setDescription('Import the CSV files from the data directory')
-            ->addOption('count', 'nu', InputOption::VALUE_OPTIONAL, 'Number of sets to import', 0)
+                ->setDescription('Import the CSV files from the data directory')
+                ->addOption('count', 'nu', InputOption::VALUE_OPTIONAL, 'Number of sets to import', 0)
         ;
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
+    protected function execute(InputInterface $input, OutputInterface $output) {
         $io = new SymfonyStyle($input, $output);
-      
+
         $end = $input->getOption('count');
-        
+
         $count = $end ? $end : 'all';
         $io->writeln("Starting to import $count sets.");
-        
-        $sets = $this->loader->loadSets(1, $end);
-       
-        $setCount = is_array($sets) ? count($sets) : $sets;
-        $io->success("Successfully imported $setCount sets");
+
+        $numberAtOnce = 500;
+        $start = 1;
+        $sets = 1;
+        $localStart = $start;
+        $localEnd = $start;
+        $io->progressStart();
+        while ((!$end || $localEnd <= $end) && $sets) {
+            $localStart = $localEnd;
+            $localEnd += $numberAtOnce;
+            if ($localEnd >= $end) {
+                $localEnd = $end + 1;
+            }
+            $sets = $this->loader->loadSets($localStart, $localEnd);
+            $setCount = is_array($sets) ? count($sets) : $sets;
+            //$io->writeln("$setCount more sets loaded");
+            $io->progressAdvance();
+        }
+        $io->progressFinish();
+        $io->success("Successfully imported some sets");
     }
+
 }
